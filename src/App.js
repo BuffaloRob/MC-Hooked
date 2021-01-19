f61483fa05dc2697f0ad3d9a83fa36d7dd2548ec
Rob Peckham <peckham.rob@gmail.com>
2021-01-19 09:15:31 -0500

    added List and item create routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './components/home/Home';
 import { Route, Switch, Router } from 'react-router-dom';
 import history from './history';
+import CreateItem from './components/items/CreateItem';
+import ListItem from './components/items/ListItem';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Router history={history}>
           <Switch>
             <Route exact path = "/" component={Home} />
+            <Route exact path="/items" component={ListItem} />
+            <Route exact path="/item/new" component={CreateItem}/>
           </Switch>
         </Router>
       </header>
